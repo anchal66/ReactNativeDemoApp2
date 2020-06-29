9d0284ff29fedafce41a518f1023e6f2b6f5aee6
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-29 22:41:41 +0530

    Rendering List of Posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,12 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-    return <BlogContext.Provider value={5}>
+    const blogPost=[
+        {title:'Blog Post 1'},
+        {title:'Blog Post 2'}
+    ]
+
+    return <BlogContext.Provider value={blogPost}>
         {children}
     </BlogContext.Provider>
 }
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,11 +1,18 @@
 import React, {useContext}from 'react';
 import { View, Text } from 'react-native';
 import BlogContext from '../context/BlogContext';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default IndexScreen = () =>{
-    const value = useContext(BlogContext);
+    const blogPost = useContext(BlogContext);
     return <View>
         <Text>Hello</Text>
-        <Text>{value}</Text>
+        <FlatList
+         data={blogPost}
+         keyExtractor={(blogPost)=>blogPost.title}
+         renderItem={({item})=>{
+             return <Text>{item.title}</Text>
+         }}
+         />
     </View>
 }
