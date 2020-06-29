96c0c467910869f61d64bc64fbe5a9d1d41bc78b
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-29 21:57:39 +0530

    Using Context: Creating Context

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { BlogProvider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator({
   Index: IndexScreen
-},{
+}, {
   initialRouteName: 'Index',
-  defaultNavigationOptions:{
+  defaultNavigationOptions: {
     title: 'Blogs'
   }
 })
 
-const App =  createAppContainer(navigator);
+const App = createAppContainer(navigator);
 
-export default ()=>{
-  return <App />
+export default () => {
+  return <BlogProvider>
+    <App />
+  </BlogProvider>
 }
