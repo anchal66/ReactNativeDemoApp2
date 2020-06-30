4846c90badd1f6ce2f9b5565c2dac46634049978
Avinash Anchal <anchalavinash66@gmail.com>
2020-06-30 11:26:50 +0530

    Updating Reducer For delete

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,13 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'delete_blogPost':
+            return state.filter((blogPost)=>blogPost.id !== action.payload)
         case 'add_blogpost':
-            return [...state, { title: `Blog Post ${state.length + 1}` }]
+            return [...state, {
+                id: Math.floor(Math.random() * 9999),
+                title: `Blog Post ${state.length + 1}`
+            }]
         default:
             return state;
     }
@@ -15,9 +20,14 @@ const addBlogPost = (dispatch) => {
         dispatch({ type: 'add_blogpost' })
     };
 };
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogPost', payload: id })
+}
+}
 
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost },
+    { addBlogPost, deleteBlogPost },
     []
 )
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,23 +1,45 @@
-import React, {useContext}from 'react';
-import { View, Text, Button } from 'react-native';
-import {Context} from '../context/BlogContext';
-import { FlatList } from 'react-native-gesture-handler';
+import React, { useContext } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
 
-export default IndexScreen = () =>{
-    const {state, addBlogPost } = useContext(Context);
+export default IndexScreen = () => {
+    const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
     return <View>
-        <Text>Index Screen</Text>
-        <Button 
+        <Button
             title="Add Post"
-            onPress={()=>addBlogPost()}
+            onPress={() => addBlogPost()}
         />
         <FlatList
-         data={state}
-         keyExtractor={(blogPost)=>blogPost.title}
-         renderItem={({item})=>{
-             return <Text>{item.title}</Text>
-         }}
-         />
+            data={state}
+            keyExtractor={(blogPost) => blogPost.title}
+            renderItem={({ item }) => {
+                return <View style={styles.row}>
+                    <Text style={styles.title}>{item.title} - {item.id}</Text>
+                    <TouchableOpacity onPress={()=>deleteBlogPost(item.id)}>
+                        <Feather style={styles.icon} name="trash" />
+                    </TouchableOpacity>
+                </View>
+            }}
+        />
     </View>
 }
+
+const styles = StyleSheet.create({
+    row: {
+        borderTopWidth: 1,
+        borderColor: 'grey',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+})
